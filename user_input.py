ff393ec0c85781f288f9b2af52d96148d322c31c
Shea50 <160406484+Shea50@users.noreply.github.com>
2024-02-24 13:47:58 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+
+age = input("What is your age: ")
+
+location = input("Where do you live: ")
+
+print("Hello Claire"   + ", you are 30 years old"  +  " and live in Canada")
+
+
